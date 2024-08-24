df54efba36781a06c47d21a9516a4a82a39bd64b
sebokdas1 <sebok.das.ff@gmail.com>
2024-08-24 22:54:38 +0600

    navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-red-500">
-      <h2 className="text-green-400">hello</h2>
-      <ul className="flex justify-between bg-black">
+    <nav className="p-3 flex justify-evenly bg-gray-200">
+      <h2 className="text-green-700 text-2xl font-bold">Logo</h2>
+      <ul className="flex justify-between space-x-4">
         <li>About</li>
         <li>Service</li>
         <li>Contact</li>
