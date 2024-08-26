585fe9ebcd8976f9b290561d9eaddf936e0ef4d4
sebokdas1 <sebok.das.ff@gmail.com>
2024-08-26 18:38:09 +0600

    custom error working at admission page

---
+++ src/app/(collegeroutes)/admission/error.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const error = () => {
+  return (
+    <div className="h-screen flex justify-center items-center bg-red-600">
+      <h1 className="text-white font-bold">admission error page...</h1>
+    </div>
+  );
+};
+
+export default error;
--- src/app/(collegeroutes)/admission/page.jsx
+++ src/app/(collegeroutes)/admission/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const page = () => {
+  console.log(roll);
   return (
     <div>
       <h1>Admission details here</h1>
