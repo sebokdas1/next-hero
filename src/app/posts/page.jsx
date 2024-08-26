b1c9da31e21d5569da749b2d171509839c6406f8
sebokdas1 <sebok.das.ff@gmail.com>
2024-08-26 21:07:26 +0600

    make a folder and page for getposts name postApi.js

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,19 +1,14 @@
+import { getPosts } from "@/postApi/postApi";
 import React from "react";
-
-const getPosts = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await res.json();
-  return data;
-};
 
 const page = async () => {
   const postsData = await getPosts();
   console.log(postsData);
   return (
     <div>
-      <div className="grid grid-cols-4 gap-6 px-2 pt-2">
+      <div className="grid grid-cols-4 gap-6 px-2 pt-2 bg-gray-300">
         {postsData?.map((post) => (
-          <div key={post.id} className="border-2 border-black px-2">
+          <div key={post.id} className="border-2 border-black px-2 bg-gray-200">
             <h3>Title: {post.title}</h3>
             <h5>Description: {post.body}</h5>
           </div>
