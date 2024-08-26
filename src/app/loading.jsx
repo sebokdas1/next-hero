b728d972718c8b6636bce43460eaefc9badb5485
sebokdas1 <sebok.das.ff@gmail.com>
2024-08-26 18:29:08 +0600

    common loading page

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="bg-green-400">
+      <h2 className="text-xl text-red-500">Common Loading....</h2>
+    </div>
+  );
+};
+
+export default loading;
