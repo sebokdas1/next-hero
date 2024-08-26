73fc2a59d0870e2f8ea5f96242dcadb85dff38de
sebokdas1 <sebok.das.ff@gmail.com>
2024-08-27 02:04:11 +0600

    remove unwanted commentout codes

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -2,14 +2,6 @@ import Meals from "@/components/Meals";
 import React from "react";
 
 const page = () => {
-  // const [meals, setmeals] = useState([]);
-
-  //   useEffect(() => {
-  //     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`);
-  //   }, [search])
-  //     .then((res) => res.json())
-  //     .then((data) => setmeals(data));
-
   return (
     <div className="bg-orange-600  pt-2">
       <h1 className="text-4xl font-bold text-white text-center">
