68c7f2389c7f0cd8196a2bea7f5cf8a853df3c85
sebokdas1 <sebok.das.ff@gmail.com>
2024-08-26 21:54:40 +0600

    get single dynamic data at dynamic route

---
+++ src/app/posts/[id]/page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const getDetailsPost = async (id) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await res.json();
+  return data;
+};
+const page = async ({ params }) => {
+  const { title, body } = await getDetailsPost(params?.id);
+  return (
+    <div className="px-3 mt-2">
+      <div className="border-2 border-blue-500 p-2">
+        <h1>Title: {title}</h1>
+        <h2>Description: {body}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,16 +1,25 @@
 import { getPosts } from "@/postApi/postApi";
+import Link from "next/link";
 import React from "react";
 
 const page = async () => {
   const postsData = await getPosts();
-  console.log(postsData);
   return (
     <div>
       <div className="grid grid-cols-4 gap-6 px-2 pt-2 bg-gray-300">
         {postsData?.map((post) => (
-          <div key={post.id} className="border-2 border-black px-2 bg-gray-200">
+          <div
+            key={post.id}
+            className="border-2 border-black px-2 bg-gray-200 py-2"
+          >
             <h3>Title: {post.title}</h3>
             <h5>Description: {post.body}</h5>
+            <Link
+              href={`/posts/${post.id}`}
+              className="text-green-700 hover:underline"
+            >
+              Details..
+            </Link>
           </div>
         ))}
       </div>
