cde6ec5404cbaf4813d8d288953d378d39b471c7
sebokdas1 <sebok.das.ff@gmail.com>
2024-08-27 19:55:43 +0600

    add keywords to metadata

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+export const metadata = {
+  title: "Services",
+  keywords: ["web development", "software", "app", "application"],
+  description: "About page for super next hero website",
+};
+
 const page = () => {
   return (
     <div>
