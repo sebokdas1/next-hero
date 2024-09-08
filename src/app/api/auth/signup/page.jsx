1bad1b55d2660db79830d67a22659df1198072fb
sebokdas1 <sebok.das.ff@gmail.com>
2024-09-09 01:46:16 +0600

    singup page styled

---
+++ src/app/api/auth/signup/page.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="rounded bg-[#f2f2f2] p-[20px] w-[50%] mt-3">
+        <form>
+          <label htmlFor="name">Name</label>
+          <input
+            className="w-[100%] py-[12px] px-[20px] my-[8px]"
+            type="text"
+            id="name"
+            placeholder="Your name"
+          />
+
+          <label htmlFor="email">Email</label>
+          <input
+            className="w-[100%] py-[12px] px-[20px] my-[8px]"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your Email "
+          />
+
+          <label htmlFor="password">Password</label>
+          <input
+            className="w-[100%] py-[12px] px-[20px] my-[8px]"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <input
+            className="w-[100%] py-[12px] px-[20px] my-[8px] bg-[#4CAF50] text-white cursor-pointer"
+            type="submit"
+            value="Register"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default page;
