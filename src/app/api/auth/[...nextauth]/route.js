f726d34daba604964c40da5fefc14ae5d85893b1
sebokdas1 <sebok.das.ff@gmail.com>
2024-09-08 23:49:49 +0600

    set token expire time, and set user custom login data

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,6 +6,7 @@ export const outhOptions = {
   secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
   session: {
     strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
   },
   providers: [
     CredentialsProvider({
@@ -40,7 +41,22 @@ export const outhOptions = {
       },
     }),
   ],
+
+  callbacks: {
+    async jwt({ token, account, user }) {
+      // Persist the OAuth access_token and or the user id to the token right after signin
+      if (account) {
+        token.type = user.type;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.type = token.type;
+      return session;
+    },
+  },
 };
+
 const handler = nextAuth(outhOptions);
 export { handler as GET, handler as POST };
 
@@ -48,13 +64,16 @@ const users = [
   {
     email: "sebok@gmail.com",
     password: "sebok",
+    type: "admin",
   },
   {
     email: "a@gmail.com",
     password: "sebok",
+    type: "modarator",
   },
   {
     email: "b@gmail.com",
     password: "sebok",
+    type: "member",
   },
 ];
