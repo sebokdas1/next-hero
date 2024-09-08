f1202a8746e807db9d0be6d838c4722daaa2681f
sebokdas1 <sebok.das.ff@gmail.com>
2024-09-09 02:21:32 +0600

    new user route created and new user api made

---
+++ src/app/api/auth/signup/new-user/route.js
@@ -0,0 +1,13 @@
+import connectDB from "@/lib/connectDB";
+
+export const POST = async (request) => {
+  try {
+    const db = await connectDB();
+    const userCollecton = db.collection("users");
+    const newUser = await request.json();
+    const res = await userCollecton.insertOne(newUser);
+    return Response.json({ message: "new user created" });
+  } catch (error) {
+    return Response.json({ message: "something went wrong" });
+  }
+};
